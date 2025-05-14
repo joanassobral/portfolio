f79f180d7e501c95f9ead2e0d14d8f5ef5d89191
joanassobral <sobral.jota@gmail.com>
2025-05-14 22:53:21 +0200

    Update May 2025
    New projects, new about, removal of language change

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ let about_btn = document.getElementById("about_btn")
 let name_btn = document.getElementById("name");
 let nav_section = document.getElementById("nav_section");
 let nav_open = true;
-name_btn.addEventListener("click",()=>{
-    if(nav_open == true){
+name_btn.addEventListener("click", () => {
+    if (nav_open == true) {
         nav_section.style.marginTop = - nav_section.offsetHeight + "px";
         nav_open = false;
         name_btn.innerText = "Joana Soares de Albergaria Ambar Sobral ↑";
         name_btn.style.textDecoration = "none";
 
-    }else if(nav_open == false) {
+    } else if (nav_open == false) {
         nav_section.style.marginTop = 0 + "px";
         nav_open = true;
         name_btn.innerText = "Joana Soares de Albergaria Ambar Sobral ↓";
@@ -21,26 +21,20 @@ name_btn.addEventListener("click",()=>{
     }
 })
 
-let currentLang = "eng"
-document.getElementById("eng_btn").classList.add("btn_selected")
 
 fetch("data.json")
 
-    .then(function(response){
+    .then(function (response) {
         return response.json();
     })
 
-    .then(function(data){
+    .then(function (data) {
 
-        for(i=0;i<data.length;i++){
+        for (i = 0; i < data.length; i++) {
             let projectId = data[i].projectId;
 
-            let engTags = data[i].engTags;
-            let esTags = data[i].esTags;
-            let ptTags = data[i].ptTags;
-            let engText = data[i].engText;
-            let esText = data[i].esText;
-            let ptText = data[i].ptText;
+            let tags = data[i].Tags;
+            let text = data[i].Text;
 
             let pageLink = data[i].link;
 
@@ -52,154 +46,115 @@ fetch("data.json")
             let x_coords_vids = data[i].x_coords_vids;
             let y_coords_vids = data[i].y_coords_vids;
 
-            function makeText(){
-
-                let lang_btn_div = document.getElementById("lang_btn_div");
-                    lang_btn_div.style.height = "auto";
-                    lang_btn_div.style.opacity = 1;
-                    lang_btn_div.style.marginTop = 1.5 + "vh"
-
-                Array.from(document.getElementsByClassName("lang_btn")).forEach(
-                    function(lang_btn){
-                        lang_btn.classList.remove("btn_selected");
-                        lang_btn.addEventListener("click",()=>{
-                            let lang_btn_id = lang_btn.id;
-                            currentLang = lang_btn_id.replace("_btn","")
-                            makeText();
-
-                            if(currentLang === "es"){
-                                about_btn.innerText = "Sobre mí";
-                                document.getElementById("project_btn_3").innerText = "Una hermosa amistad con Spotify";
-                            } else if(currentLang === "eng") {
-                                about_btn.innerText = "About";
-                                document.getElementById("project_btn_3").innerText = "A beautiful friendship with Spotify";
-                            } else if(currentLang === "pt") {
-                                about_btn.innerText = "Sobre mim";
-                                document.getElementById("project_btn_3").innerText = "Uma linda amizade com o Spotify";
-                            };;
-                        })
-                    }
-                )
-                let currentText = eval(currentLang + "Text");
-                let currentTags = eval(currentLang + "Tags");
-                let currentLangBtn = eval(currentLang + "_btn")
-                    currentLangBtn.classList.add("btn_selected");
-
-
+            function makeText() {
                 project_tags_div.innerHTML = "";
-                if(engTags){
+                if (tags) {
                     project_tags_div.style.marginBottom = 4 + "vh";
 
-                    const tagsArray = currentTags.split(";");
-                        for(k=0;k<tagsArray.length;k++){
-                            let tag = document.createElement("p");
-                                tag.innerText = "◊ " + tagsArray[k];
-                                project_tags_div.appendChild(tag)
-                        }
-                } else if(!engTags) {
+                    const tagsArray = tags.split(";");
+                    for (k = 0; k < tagsArray.length; k++) {
+                        let tag = document.createElement("p");
+                        tag.innerText = "◊ " + tagsArray[k];
+                        project_tags_div.appendChild(tag)
+                    }
+                } else if (!tags) {
                     project_tags_div.style.marginBottom = 0;
                 }
-                if(pageLink){
+                if (pageLink) {
                     let link = document.createElement("p");
-                        link.classList.add("link")
-                        if(currentLang === "eng"){
-                            link.innerText = "Visit website →"
-                        } else if(currentLang === "es"){
-                            link.innerText = "Visita la página →"
-                        } else if(currentLang === "pt"){
-                            link.innerText = "Visita a página →"
-                        }
-                        project_tags_div.appendChild(link);
-                        link.addEventListener("click",()=>{
-                            window.open(pageLink,"_blank")
-                        })
+                    link.classList.add("link")
+                    link.innerText = "Visit page →"
+                    project_tags_div.appendChild(link);
+                    link.addEventListener("click", () => {
+                        window.open(pageLink, "_blank")
+                    })
                 }
 
                 project_text_div.innerHTML = "";
-                const textArray = currentText.split(";");
-                for(l=0;l<textArray.length;l++){
+                const textArray = text.split(";");
+                for (l = 0; l < textArray.length; l++) {
                     let textPrg = document.createElement("p");
-                        textPrg.innerText = textArray[l];
-                        project_text_div.appendChild(textPrg)
+                    textPrg.innerText = textArray[l];
+                    project_text_div.appendChild(textPrg)
                 }
             }
 
             let button = document.getElementById(projectId);
-                button.addEventListener("click",()=>{
-                    
-                    about_btn.classList.remove("tiny_nudge_animation");
+            button.addEventListener("click", () => {
 
-                    window.scrollTo(0,0)
+                about_btn.classList.remove("tiny_nudge_animation");
 
-                    currentProject = projectId;
+                window.scrollTo(0, 0)
 
-                    Array.from(document.getElementsByClassName("btn")).forEach(
-                        function(btn){
-                                    btn.classList.remove("btn_selected");
-                                }
-                    )
-                    button.classList.add("btn_selected");
-                    makeText();
+                currentProject = projectId;
 
-                    scroll_body.innerHTML = "";
+                Array.from(document.getElementsByClassName("btn")).forEach(
+                    function (btn) {
+                        btn.classList.remove("btn_selected");
+                    }
+                )
+                button.classList.add("btn_selected");
+                makeText();
 
-                    for(m=0;m<numberImgs;m++){
-                        let image_spacer_div = document.createElement("div");
-                            image_spacer_div.classList.add("spacer_div");
-                            scroll_body.appendChild(image_spacer_div)
+                scroll_body.innerHTML = "";
 
-                        let project_img = document.createElement("img");
-                            project_img.src = "Media/" + projectId + "/" + m + ".png"
-                            
-                            const x_coords_array = x_coords_imgs.split(",");
-                            const y_coords_array = y_coords_imgs.split(",");
-                            image_spacer_div.style.left = x_coords_array[m] + "vh";
-                            image_spacer_div.style.top = y_coords_array[m] + "vh";
+                for (m = 0; m < numberImgs; m++) {
+                    let image_spacer_div = document.createElement("div");
+                    image_spacer_div.classList.add("spacer_div");
+                    scroll_body.appendChild(image_spacer_div)
 
-                            if(currentProject === "project_btn_1" || currentProject === "project_btn_2"){
-                                project_img.classList.add("long_img")
-                            }
+                    let project_img = document.createElement("img");
+                    project_img.src = "Media/" + projectId + "/" + m + ".png"
 
-                            image_spacer_div.appendChild(project_img)
+                    const x_coords_array = x_coords_imgs.split(",");
+                    const y_coords_array = y_coords_imgs.split(",");
+                    image_spacer_div.style.left = x_coords_array[m] + "vh";
+                    image_spacer_div.style.top = y_coords_array[m] + "vh";
 
-                            if(window.innerHeight>window.innerWidth){
-                                image_spacer_div.style.left = (x_coords_array[m]*0.70) + "vh";
-                                image_spacer_div.style.top = (y_coords_array[m]*0.70) + "vh";
-                            }
+                    if (currentProject === "project_btn_3" || currentProject === "project_btn_4") {
+                        project_img.classList.add("long_img")
                     }
 
-                    for(n=0;n<numberVids;n++){
-                        let video_spacer_div = document.createElement("div");
-                            video_spacer_div.classList.add("spacer_div");
-                            scroll_body.appendChild(video_spacer_div)
+                    image_spacer_div.appendChild(project_img)
 
-                            let video = document.createElement("video");
-                                video.src = "Media/" + projectId + "/" + n + ".mp4"
-                                video.autoplay = true;
-                                video.loop = true;
-                                video.playsInline = true;
-                                video.muted = true;
-                                video_spacer_div.appendChild(video)
-                                
-                                const x_coords_array = x_coords_vids.split(",");
-                                const y_coords_array = y_coords_vids.split(",");
-                                video_spacer_div.style.left = x_coords_array[n] + "vh";
-                                video_spacer_div.style.top = y_coords_array[n] + "vh";
-
-                                if(currentProject === "about_btn"){
-                                    video.id = "about_video";
-                                }
-
-                                if(window.innerHeight>window.innerWidth){
-                                    video_spacer_div.style.left = (x_coords_array[n]*0.70) + "vh";
-                                    video_spacer_div.style.top = (y_coords_array[n]*0.70) + "vh";
-                                }
+                    if (window.innerHeight > window.innerWidth) {
+                        image_spacer_div.style.left = (x_coords_array[m] * 0.70) + "vh";
+                        image_spacer_div.style.top = (y_coords_array[m] * 0.70) + "vh";
                     }
-                })
+                }
+
+                for (n = 0; n < numberVids; n++) {
+                    let video_spacer_div = document.createElement("div");
+                    video_spacer_div.classList.add("spacer_div");
+                    scroll_body.appendChild(video_spacer_div)
+
+                    let video = document.createElement("video");
+                    video.src = "Media/" + projectId + "/" + n + ".mp4"
+                    video.autoplay = true;
+                    video.loop = true;
+                    video.playsInline = true;
+                    video.muted = true;
+                    video_spacer_div.appendChild(video)
+
+                    const x_coords_array = x_coords_vids.split(",");
+                    const y_coords_array = y_coords_vids.split(",");
+                    video_spacer_div.style.left = x_coords_array[n] + "vh";
+                    video_spacer_div.style.top = y_coords_array[n] + "vh";
+
+                    if (currentProject === "about_btn") {
+                        video.id = "about_video";
+                    }
+
+                    if (window.innerHeight > window.innerWidth) {
+                        video_spacer_div.style.left = (x_coords_array[n] * 0.70) + "vh";
+                        video_spacer_div.style.top = (y_coords_array[n] * 0.70) + "vh";
+                    }
+                }
+            })
         }
     })
 
-    .catch(function(err){
+    .catch(function (err) {
         console.log(err);
     });
 
